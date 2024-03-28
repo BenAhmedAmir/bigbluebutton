874b5e879c6ada99297016c7106466aaf506f615
BenAhmedAmir <benahmedamir@hotmail.fr>
2024-03-28 09:18:47 +0100

    mute by moderator

--- akka-bbb-apps/src/main/scala/org/bigbluebutton/core/apps/users/MuteUserCmdMsgHdlr.scala
+++ akka-bbb-apps/src/main/scala/org/bigbluebutton/core/apps/users/MuteUserCmdMsgHdlr.scala
@@ -15,7 +15,6 @@ trait MuteUserCmdMsgHdlr extends RightsManagementTrait {
 
   def handleMuteUserCmdMsg(msg: MuteUserCmdMsg) {
     val unmuteDisabled = !liveMeeting.props.usersProp.allowModsToUnmuteUsers && msg.body.mute == false
-    var mutedByModeratorSet: Set[String] = Set()
     if (msg.body.userId != msg.header.userId && (unmuteDisabled || permissionFailed(
       PermissionCheck.MOD_LEVEL,
       PermissionCheck.VIEWER_LEVEL, liveMeeting.users2x, msg.header.userId
@@ -27,7 +26,7 @@ trait MuteUserCmdMsgHdlr extends RightsManagementTrait {
     } else {
       val meetingId = liveMeeting.props.meetingProp.intId
       val voiceConf = liveMeeting.props.voiceProp.voiceConf
-      log.info("you are muted by moderator")
+
       log.info("Received mute user request. meetingId=" + meetingId + " userId="
         + msg.body.userId)
 
@@ -43,10 +42,12 @@ trait MuteUserCmdMsgHdlr extends RightsManagementTrait {
         )
       } yield {
 
-//        var mutedByModerator: Boolean = false
-
-        if (requester.role != Roles.MODERATOR_ROLE && permissions.disableMic && requester.locked && u.muted && msg.body.userId == msg.header.userId) {
-          // Non-moderator user trying to unmute another user of lower role while microphone is disabled. Do not allow.
+        if (requester.role != Roles.MODERATOR_ROLE
+          && permissions.disableMic
+          && requester.locked
+          && u.muted &&
+          msg.body.userId == msg.header.userId) {
+          // unmuting self while not moderator and mic disabled. Do not allow.
         } else {
           if (u.muted != msg.body.mute) {
             log.info("Send mute user request. meetingId=" + meetingId + " userId=" + u.intId + " user=" + u)
@@ -57,34 +58,8 @@ trait MuteUserCmdMsgHdlr extends RightsManagementTrait {
               msg.body.mute
             )
             outGW.send(event)
-            var mutedByModerator = u.mutedByModerator
-
-            // Update the mutedByModerator flag if the moderator mutes the user
-            if (requester.role == Roles.MODERATOR_ROLE && msg.body.mute) {
-              log.info("################################")
-              log.info("################################")
-              log.info("################################")
-              log.info(u.mutedByModerator.toString)
-              mutedByModerator = true
-              log.info(u.mutedByModerator.toString)
-              log.info("################################")
-              log.info("################################")
-              log.info("################################")
-            }
           }
         }
-
-        // Prevent self-unmuting if the user was muted by a moderator
-        if (u.mutedByModerator && msg.body.userId == msg.header.userId && !msg.body.mute) {
-          // Muted by moderator, and trying to unmute oneself. Do not allow.
-          log.info("################################")
-          log.info("################################")
-          log.info("################################")
-          log.info("you can not open microphone")
-          log.info("################################")
-          log.info("################################")
-        }
-
       }
     }
 
