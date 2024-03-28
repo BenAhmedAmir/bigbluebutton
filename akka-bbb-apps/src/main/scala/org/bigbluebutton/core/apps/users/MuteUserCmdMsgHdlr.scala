421b54a8665976173a54ebd4c87e66162571f25f
BenAhmedAmir <benahmedamir@hotmail.fr>
2024-03-28 10:45:38 +0100

    mute by moderator

--- akka-bbb-apps/src/main/scala/org/bigbluebutton/core/apps/users/MuteUserCmdMsgHdlr.scala
+++ akka-bbb-apps/src/main/scala/org/bigbluebutton/core/apps/users/MuteUserCmdMsgHdlr.scala
@@ -64,7 +64,7 @@ trait MuteUserCmdMsgHdlr extends RightsManagementTrait {
               log.info("################################")
               log.info("################################")
               log.info("################################")
-              log.info(u.mutedByModerator.toString)
+              log.info(u)
               mutedByModerator = true
               log.info(u.mutedByModerator.toString)
               log.info("################################")
