abf626e9b95b2bd1ed075a2462132ff94f60cffd
BenAhmedAmir <benahmedamir@hotmail.fr>
2024-04-01 14:32:21 +0100

    prevent userr to unmute

--- akka-bbb-apps/src/main/scala/org/bigbluebutton/core/apps/users/MuteUserCmdMsgHdlr.scala
+++ akka-bbb-apps/src/main/scala/org/bigbluebutton/core/apps/users/MuteUserCmdMsgHdlr.scala
@@ -1,62 +1,46 @@
-package org.bigbluebutton.core.apps.users
+def handleMuteUserCmdMsg(msg: MuteUserCmdMsg) {
+  val unmuteDisabled = !liveMeeting.props.usersProp.allowModsToUnmuteUsers && msg.body.mute == false
+  if (msg.body.userId != msg.header.userId && (unmuteDisabled || permissionFailed(
+    PermissionCheck.MOD_LEVEL,
+    PermissionCheck.VIEWER_LEVEL, liveMeeting.users2x, msg.header.userId
+  ))) {
+    val meetingId = liveMeeting.props.meetingProp.intId
+    val muteUnmuteStr: String = if (msg.body.mute) "mute" else "unmute"
+    val reason = "No permission to " + muteUnmuteStr + " user."
+    PermissionCheck.ejectUserForFailedPermission(meetingId, msg.header.userId, reason, outGW, liveMeeting)
+  } else {
+    val meetingId = liveMeeting.props.meetingProp.intId
+    val voiceConf = liveMeeting.props.voiceProp.voiceConf
 
-import org.bigbluebutton.common2.msgs.MuteUserCmdMsg
-import org.bigbluebutton.core.apps.{PermissionCheck, RightsManagementTrait}
-import org.bigbluebutton.core.models.{Roles, Users2x, VoiceUsers}
-import org.bigbluebutton.core.running.{LiveMeeting, OutMsgRouter}
-import org.bigbluebutton.core2.MeetingStatus2x
-import org.bigbluebutton.core2.message.senders.MsgBuilder
+    log.info("Received mute user request. meetingId=" + meetingId + " userId="
+      + msg.body.userId)
 
-trait MuteUserCmdMsgHdlr extends RightsManagementTrait {
-  this: UsersApp =>
-
-  val liveMeeting: LiveMeeting
-  val outGW: OutMsgRouter
-
-  def handleMuteUserCmdMsg(msg: MuteUserCmdMsg) {
-    val unmuteDisabled = !liveMeeting.props.usersProp.allowModsToUnmuteUsers && msg.body.mute == false
-    if (msg.body.userId != msg.header.userId && (unmuteDisabled || permissionFailed(
-      PermissionCheck.MOD_LEVEL,
-      PermissionCheck.VIEWER_LEVEL, liveMeeting.users2x, msg.header.userId
-    ))) {
-      val meetingId = liveMeeting.props.meetingProp.intId
-      val muteUnmuteStr: String = if (msg.body.mute) "mute" else "unmute"
-      val reason = "No permission to " + muteUnmuteStr + " user."
-      PermissionCheck.ejectUserForFailedPermission(meetingId, msg.header.userId, reason, outGW, liveMeeting)
-    } else {
-      val meetingId = liveMeeting.props.meetingProp.intId
-      val voiceConf = liveMeeting.props.voiceProp.voiceConf
-
-      log.info("Received mute user request. meetingId=" + meetingId + " userId="
-        + msg.body.userId)
-
-      val permissions = MeetingStatus2x.getPermissions(liveMeeting.status)
-      for {
-        requester <- Users2x.findWithIntId(
-          liveMeeting.users2x,
-          msg.header.userId
-        )
-        u <- VoiceUsers.findWithIntId(
-          liveMeeting.voiceUsers,
-          msg.body.userId
-        )
-      } yield {
-
-        if (requester.role != Roles.MODERATOR_ROLE
-          && u.muted &&
-          msg.body.userId == msg.header.userId) {
-          // unmuting self while not moderator and was muted by a moderator. Do not allow.
-        } else {
-          if (u.muted != msg.body.mute) {
-            log.info("Send mute user request. meetingId=" + meetingId + " userId=" + u.intId + " user=" + u)
-            val event = MsgBuilder.buildMuteUserInVoiceConfSysMsg(
-              meetingId,
-              voiceConf,
-              u.voiceUserId,
-              msg.body.mute
-            )
-            outGW.send(event)
-          }
+    val permissions = MeetingStatus2x.getPermissions(liveMeeting.status)
+    for {
+      requester <- Users2x.findWithIntId(
+        liveMeeting.users2x,
+        msg.header.userId
+      )
+      u <- VoiceUsers.findWithIntId(
+        liveMeeting.voiceUsers,
+        msg.body.userId
+      )
+    } yield {
+      if (requester.role != Roles.MODERATOR_ROLE && mutedBy == Roles.MODERATOR_ROLE && u.muted && msg.body.userId == msg.header.userId) {
+        // unmuting self while not moderator and was muted by a moderator. Do not allow.
+      } else {
+        if (u.muted != msg.body.mute) {
+          log.info("Send mute user request. meetingId=" + meetingId + " userId=" + u.intId + " user=" + u)
+          // Set mutedBy to MODERATOR_ROLE if muted by moderator
+          val mutedBy = if (u.muted && requester.role == Roles.MODERATOR_ROLE) Roles.MODERATOR_ROLE else u.mutedBy
+          val event = MsgBuilder.buildMuteUserInVoiceConfSysMsg(
+            meetingId,
+            voiceConf,
+            u.voiceUserId,
+            msg.body.mute,
+            mutedBy
+          )
+          outGW.send(event)
         }
       }
     }
