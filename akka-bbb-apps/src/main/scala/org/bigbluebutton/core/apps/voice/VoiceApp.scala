04624930488328adce93502da544a53ff55b60e9
amir <benahmedamir@hotmail.fr>
2024-10-23 16:03:05 +0100

    allow viewer to unmute if he is muted by his own

--- akka-bbb-apps/src/main/scala/org/bigbluebutton/core/apps/voice/VoiceApp.scala
+++ akka-bbb-apps/src/main/scala/org/bigbluebutton/core/apps/voice/VoiceApp.scala
@@ -112,7 +112,7 @@ object VoiceApp extends SystemConfiguration {
       muted:       Boolean
   )(implicit context: ActorContext): Unit = {
     for {
-      mutedUser <- VoiceUsers.userMuted(liveMeeting.voiceUsers, voiceUserId, muted)
+      mutedUser <- VoiceUsers.userMuted(liveMeeting.voiceUsers, voiceUserId, muted,voiceUserId)
     } yield {
       if (!muted) {
         // Make sure lock settings are in effect (ralam dec 6, 2019)
