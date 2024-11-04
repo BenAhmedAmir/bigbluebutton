cec9600d0eee917cc6fbba524b93446a3572b80e
Hama <mohamedbenjedida18@gmail.com>
2024-11-04 10:07:48 +0100

    added logs

--- bigbluebutton-html5/imports/ui/components/chat/container.jsx
+++ bigbluebutton-html5/imports/ui/components/chat/container.jsx
@@ -137,7 +137,7 @@ const ChatContainer = (props) => {
     sysMessagesIds.welcomeId,
     amIModerator && modOnlyMessage && sysMessagesIds.moderatorId,
   ].filter((i) => i);
-
+  console.log('currentUser', currentUser);
   const usingChatContext = useContext(ChatContext);
   const usingGroupChatContext = useContext(GroupChatContext);
   const [stateLastMsg, setLastMsg] = useState(null);
