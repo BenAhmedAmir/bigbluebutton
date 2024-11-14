780a1ebe7c3102d41a5ec0ba3b2f1fdcbb9828bc
Hama <mohamedbenjedida18@gmail.com>
2024-11-14 09:53:58 +0100

    fixing mute all

--- bigbluebutton-html5/imports/api/voice-users/server/methods/muteAllExceptPresenterToggle.js
+++ bigbluebutton-html5/imports/api/voice-users/server/methods/muteAllExceptPresenterToggle.js
@@ -21,7 +21,7 @@ export default async function muteAllExceptPresenterToggle() {
 
     const payload = {
       mutedBy: requesterUserId,
-      mute: false,
+      mute: toggleMeetingMuted,
     };
 
     RedisPubSub.publishUserMessage(CHANNEL, EVENT_NAME, meetingId, requesterUserId, payload);
